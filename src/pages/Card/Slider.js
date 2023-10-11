14268fca2b2b3e228b5dbbadd473c10fd57e6ea9
ItSushko14 <ItSushkoAndriy@gmail.com>
2023-10-11 18:12:35 +0300

    add slider

--- src/pages/Card/Slider.js
+++ src/pages/Card/Slider.js
@@ -1,26 +1,68 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './card.module.css';
 import Slider from '@mui/material/Slider';
+import Button from '@mui/material/Button';
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
 
 export const SliderCount = (props) => {
 
-    const [sliderValue, setSliderValue] = useState(30);
+    
+    const [sliderValue, setSliderValue] = useState(props.total);
+    const [sliderStep, setSliderStep] = useState(50)
+    const [maxValue, setMaxValue] = useState(props.total * 20);
 
     const handleSliderChange = (event, newValue) => {
         setSliderValue(newValue);
+        
     };
+
     
+    
+    const recalculate = (items, total) => {
+        const newItems = {}
+        items.map( (item) => {
+
+            newItems[item.name] = item.quantity
+            
+        })
+        console.log(newItems)
+    }
+    recalculate(props.items, props.total)
+
+    useEffect(() => {
+        // Встановлюємо максимальне значення один раз при завантаженні компонента
+        setMaxValue(props.total * 20);
+    
+    }, [props.total]);
+
 	return (
 		<>
             <div className={styles.sliderContainer}>
-                <button onClick={() => setSliderValue(sliderValue + 1)}>+</button>
+                <Button 
+                    className={styles.sliderButton}
+                    variant="outlined" 
+                    size="small" 
+                    onClick={() => setSliderValue(sliderValue + sliderStep)}
+                >
+                    <AddIcon />
+                </Button>
                 <Slider 
                     orientation="vertical"
-                    defaultValue={30} 
                     value={sliderValue}
                     step={50}
+                    onChange={handleSliderChange}
+                    max={maxValue}
+                    min={0}
                 />
-                <button onClick={() => setSliderValue(sliderValue - 1)}>-</button>
+                <Button 
+                    className={styles.sliderButton}
+                    variant="outlined" 
+                    size="small" 
+                    onClick={() => setSliderValue(sliderValue - sliderStep)}
+                >
+                    <RemoveIcon />
+                </Button>
             </div>
             
 		</>
